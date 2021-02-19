1514579c3b577fc161bcdd9d33de4887d4c1207f
jack-borg <jacob.fam.borg@gmail.com>
2021-02-19 13:59:18 +0000

    test

--- commands/test.js
+++ commands/test.js
@@ -9,11 +9,11 @@ module.exports = {
 	execute(msg, args, dao) {
 		if (msg.author.id !== process.env.DEVID) return;
 
-		// const role = msg.guild.roles.cache.get('801445893210636299');
+		//console.log(msg.guild.roles.cache.map((role) => role.name + ': ' + role.id));
 		console.log(
 			msg.guild.members.cache
-				.filter((m) => m._roles.includes('801445893210636299'))
-				.map((m) => `${m.user.username}: ${m.id}`)
+				.filter((m) => m._roles.includes('745898067924091001')).size
+				//.map((m) => `${m.user.username}: ${m.id}`)
 			// .join('\n')
 		);
 	},
