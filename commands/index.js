62e1af4220922b5fd8d43fd9e0c2b422fb505764
jack-borg <jacob.fam.borg@gmail.com>
2021-03-08 00:23:31 +0100

    warnum command to change current war

--- commands/index.js
+++ commands/index.js
@@ -1,7 +1,9 @@
 module.exports = {
 	Stats: require('./CWStats'),
+	Warnum: require('./warnum'),
+	// Outcome: require('./outcome'),
 	// Accept: require('./accept'),
-	// Apply: require('./apply'),
+	Apply: require('./apply'),
 	// Decline: require('./decline'),
 	// Next: require('./next'),
 	Test: require('./test'),
---
+++ commands/warnum.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const fs = require('fs');
+
+module.exports = {
+	name: 'grz.warnum',
+	description: 'Dev cmd to change war number',
+	execute(msg, args, bot) {
+		if (msg.author.id !== process.env.DEVID) return;
+
+		if (args.length !== 1) return msg.channel.send('Current war is ' + process.env.CURRENTWAR);
+
+		const value = args[0];
+
+		fs.readFile('./.env', 'utf8', (err, data) => {
+			if (err) return console.error(err);
+
+			env = data.split('\n');
+
+			let target = env.findIndex((f) => f.split('=')[0] == 'CURRENTWAR');
+
+			env[target] = env[target].split('=')[0] + '=' + value;
+
+			fs.writeFile('./.env', env.join('\n'), function (err) {
+				if (err) return console.log(err);
+				msg.channel.send('Current war changed to ' + value);
+			});
+		});
+	},
+};
